0ac21e2cc201a3097f3da281629f0d73b3a7b803
Dan Hart <danhart@notonthehighstreet.com>
2013-01-08 11:51:00 +0000

    Update install script to use string paths

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 function ensure_link {
-  test -L $2 || ln -s $1 $2
+  test -L "$2" || ln -s "$1" "$2"
 }
 
-ensure_link ${PWD}/bash_profile ~/.bash_profile
-ensure_link ${PWD}/ctags ~/.ctags
-ensure_link ${PWD}/vimrc ~/.vimrc
-ensure_link ${PWD}/vim ~/.vim
-ensure_link ${PWD}/ackrc ~/.ackrc
-ensure_link ${PWD}/gitconfig ~/.gitconfig
-ensure_link ${PWD}/gitignore ~/.gitignore
+ensure_link "${PWD}/bash_profile" "$HOME/.bash_profile"
+ensure_link "${PWD}/ctags" "$HOME/.ctags"
+ensure_link "${PWD}/vimrc" "$HOME/.vimrc"
+ensure_link "${PWD}/vim" "$HOME/.vim"
+ensure_link "${PWD}/ackrc" "$HOME/.ackrc"
+ensure_link "${PWD}/gitconfig" "$HOME/.gitconfig"
+ensure_link "${PWD}/gitignore" "$HOME/.gitignore"
 
-ensure_link ${PWD}/bin ~/bin
+ensure_link "${PWD}/bin" "$HOME/bin"
 
